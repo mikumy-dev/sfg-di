c199afba6985efd20d7bb5cc1696debd1647375d
zosoyi <cjsz321qq@gmail.com>
2022-01-10 19:59:40 +0900

    32. The Spring Context
    Create a simple injection use the Spring Context

--- src/main/java/guru/springframwork/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframwork/sfgdi/SfgDiApplication.java
@@ -1,13 +1,21 @@
 package guru.springframwork.sfgdi;
 
+import guru.springframwork.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+        // ask Spring to create the instance of that Object
+        MyController myController = (MyController) context.getBean("myController");
+
+        // and use that Object to execute the sayHello method
+        System.out.println(myController.sayHello());
     }
 
 }
---
+++ src/main/java/guru/springframwork/sfgdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package guru.springframwork.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!!!");
+
+        return "Hi, folks!";
+    }
+}
